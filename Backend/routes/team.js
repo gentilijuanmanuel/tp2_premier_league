c12e2ef7a5af7b53b5eb78c2db416e41029a4b18
Martin Bianchi <tinchola7@gmail.com>
2017-11-07 23:35:52 -0300

    Update and GetAll teams

--- Backend/routes/team.js
+++ Backend/routes/team.js
@@ -4,6 +4,8 @@ var Team = mongoose.model('Team');
 
 var ObjectId = mongoose.Types.ObjectId;
 
+
+//Insert new team
 router.post('/', (req, res, next) => {
     let stadium=req.body.stadium;
     let name=req.body.name;
@@ -17,9 +19,34 @@ router.post('/', (req, res, next) => {
     res.send("post client:"+stadium+" - name:"+name+team.name);
   });
 
-router.get('/', function(req, res, next) {
-    res.send("Equipos");
+//Return all teams
+router.get('/', (req, res, next)=> {
+    Team.find({}).then(teams =>{
+        if(!teams){return res.sendStatus(401);}
+        return res.json({'teams': teams})
 })
+.catch(next);
+});
 
+
+//Update Team
+//Lo hice por ID porque por Name no me funcionaba, creo que el campo tiene que ser declarado como unique para poder
+//usar el metodo findOneAndUpdate, habria que modificar el modelo.
+router.put('/:id', (req, res, next) =>{
+    let query = {"_id": req.params.id};
+    let name = req.body.name;
+    let stadium = req.body.stadium;
+    let points = req.body.points;
+
+    Team.findOneAndUpdate(query, {$set: {name: name, stadium:stadium, points:points}},{new: true},function(err, team){
+        if(err){
+            res.send("got an error");
+        }
+        else{
+            res.send(team);                
+        }
+    });
+
+})
 
 module.exports=router;
